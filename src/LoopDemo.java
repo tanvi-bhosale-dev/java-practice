68627c6844ee29c6e6274c87ff074fb8d9b37e49
tanvi-bhosale-dev <tanvibharatbhosale2506@gmail.com>
2026-01-24 15:09:33 +0530

    Create LoopDemo.java

---
+++ src/LoopDemo.java
@@ -0,0 +1,10 @@
+public class LoopDmo
+  {
+    public static void main(String[]args)
+    {
+      for (int i = 1;i <= 5;i++)
+        {
+          SYstem.out.println("number :"+i);
+        }
+    }
+  }
