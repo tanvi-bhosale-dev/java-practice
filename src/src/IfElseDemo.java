d02620e18f0d5cb1ce41e2e5e860b68b03a1db02
tanvi-bhosale-dev <tanvibharatbhosale2506@gmail.com>
2026-01-24 14:55:10 +0530

    Create IfElseDemo.java

---
+++ src/src/IfElseDemo.java
@@ -0,0 +1,15 @@
+public class IfElseDemo 
+  {
+    public static void main(String[]args)
+    {
+      int number =10;
+
+      if (number > 0)
+      {
+        System.out.println("positive number");
+      } else {
+        System.out.println("negative number");
+      }
+    }
+  }
+      
