9bd5791bd5c2d876fbdba17eef4d5e86c85770fb
tanvi-bhosale-dev <tanvibharatbhosale2506@gmail.com>
2026-05-14 15:16:39 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+class DataTypes
+{
+	public static void main(String args[])
+	{
+		int age = 20;
+		double percentage = 85.75;
+		char grade = 'A';
+		boolean pass = true;
+
+		System.out.println("Age = "+age);
+		System.out.println("Percentage = "+percentage);
+		System.out.println("Grade = "+grade);
+		System.out.println("Pass = "+pass);
+	}	
+}
